85396d5c3b829133753f64e960553c7a57524c4d
aqsa <iaqsa6@gmail.com>
2026-07-29 13:05:58 +0530

    added appointment files

---
+++ appointment.js
@@ -0,0 +1,31 @@
+function bookAppointment(){
+
+let patient=document.getElementById("patientName").value;
+let age=document.getElementById("age").value;
+let doctor=document.getElementById("doctor").value;
+let date=document.getElementById("date").value;
+let time=document.getElementById("time").value;
+
+if(patient=="" || age=="" || doctor=="" || date=="" || time==""){
+alert("Please fill all fields.");
+return;
+}
+
+let table=document.getElementById("appointmentTable");
+
+let row=table.insertRow();
+
+row.insertCell(0).innerHTML=patient;
+row.insertCell(1).innerHTML=age;
+row.insertCell(2).innerHTML=doctor;
+row.insertCell(3).innerHTML=date;
+row.insertCell(4).innerHTML=time;
+
+document.getElementById("patientName").value="";
+document.getElementById("age").value="";
+document.getElementById("doctor").value="";
+document.getElementById("date").value="";
+document.getElementById("time").value="";
+
+alert("Appointment Booked Successfully!");
+}
